8daee3ee69e0ec58ebb716b8c482d001c5864e38
dusterherz <dusterherz@gmail.com>
2015-11-28 02:09:35 -0500

    Auth is working with marshmallow now !

--- nestedworld_api/views/api/v1/user/auth.py
+++ nestedworld_api/views/api/v1/user/auth.py
@@ -1,6 +1,5 @@
-from flask.ext.restplus import Resource
-from flask.ext.restplus import fields
-from nestedworld_api.db import db
+from marshmallow import post_dump
+from nestedworld_api.app import ma
 from nestedworld_api.login import get_session, login_required
 from . import user
 
@@ -12,45 +11,119 @@ auth = user.namespace('auth')
 
 
 @auth.route('/login/simple')
-class Login(Resource):
+class Login(auth.Resource):
 
     '''
         Simple login endpoint.
         Only use user/password pair and the application token.
     '''
 
-    def post(self):
+    class UserSchema(ma.Schema):
+        email = ma.Email()
+        password = ma.String()
+        app_token = ma.String()
+        data = ma.Raw()
+
+    class SessionSchema(ma.Schema):
+
+        token = ma.String()
+
+    @auth.accept(UserSchema(partial=True))
+    @auth.marshal_with(SessionSchema())
+    def post(self, data):
         '''
             Simple login with user/password pair and the application token.
         '''
-        auth.abort(501)
+        from nestedworld_api.db import db
+        from nestedworld_api.db import User, Application, Session
+
+        user = User.query.filter(
+            User.email == data['email'], User.is_active == True).first()
+        if user is None or user.password != data['password']:
+            auth.abort(400, message='Cannot connect: Wrong email and/or password.')
+
+        app = Application.query.filter(
+            Application.token == data['app_token']).first()
+        if app is None:
+            auth.abort(400, message='Bad application token')
+
+        session = Session(application=app, user=user, data=data.get('data'))
+        db.session.add(session)
+        db.session.commit()
+
+        return session
 
 # Register
 
 @auth.route('/register')
-class Register(Resource):
+class Register(auth.Resource):
 
-    def post(self):
+    class Schema(ma.Schema):
+        email = ma.Email()
+        password = ma.String()
+        pseudo = ma.String()
+
+    @auth.accept(Schema())
+    @auth.marshal_with(Schema())
+    def post(self, data):
         '''
             Register a user.
         '''
-        auth.abort(501)
+        from nestedworld_api.db import db
+        from nestedworld_api.db import Application, User
+
+
+        user = User.query.filter(User.email == data['email']).first()
+        if user is not None:
+            auth.abort(409, 'User already exists')
+
+        user = User()
+        user.email = data['email']
+        user.password = data['password']
+        user.pseudo = data['pseudo']
+
+        db.session.add(user)
+        db.session.commit()
+
+        return user
 
 # Lost password
 
 @auth.route('/resetpassword')
-class ResetPassword(Resource):
+class ResetPassword(auth.Resource):
 
-    def post(self):
+    class Schema(ma.Schema):
+        email = ma.Email()
+
+    @auth.accept(Schema())
+    def post(self, data):
         '''
             Request a password reset.
         '''
-        auth.abort(501)
+        
+        from nestedworld_api.db import db
+        from nestedworld_api.db import User, PasswordResetRequest
+        from nestedworld_api.mail import TemplatedMessage
+
+        user = User.query.filter(
+            User.email == data['email'], User.is_active == True).first()
+        if user is None:
+            auth.abort(400, 'User not found')
+
+        request = PasswordResetRequest(user=user)
+        db.session.add(request)
+        db.session.commit()
+
+        message = TemplatedMessage('mail/password_reset.txt', token=request.token)
+        message.add_recipient(user.email)
+        message.send()
+
+        return 'OK'
 
 # Logout
 
 @auth.route('/logout')
-class Logout(Resource):
+class Logout(auth.Resource):
 
     @login_required
     def get(self):
