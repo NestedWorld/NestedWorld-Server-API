5148e857c0001b5e2e21d3238b83aebd98729f20
KokaKiwi <kokakiwi@kokakiwi.net>
2015-12-09 00:40:33 +0900

    Generate Swagger spec file from views and schemas.

--- flask_guimauve/api.py
+++ flask_guimauve/api.py
@@ -1,15 +1,21 @@
 from flask.ext import restful
+from . import swagger
 from .shortcuts import Shortcuts
 
 class Api(restful.Api, Shortcuts):
 
     def __init__(self, spec, app=None, **kwargs):
-        super().__init__(app, **kwargs)
         self.spec = spec
 
         # Add necessary plugins to spec
-        # self.spec.setup_plugin('apispec.ext.flask')
-        # self.spec.setup_plugin('apispec.ext.marshmallow')
+        self.spec.setup_plugin('apispec.ext.marshmallow')
+
+        super().__init__(app, **kwargs)
+
+    def init_app(self, app):
+        super().init_app(app)
+
+        self.add_resource(self.swagger_view(), '/swagger.json', endpoint='specs')
 
     def namespace(self, *args, **kwargs):
         from .namespace import Namespace
@@ -18,4 +24,13 @@ class Api(restful.Api, Shortcuts):
 
     def add_resource(self, resource, *urls, **kwargs):
         super().add_resource(resource, *urls, **kwargs)
-        print(resource)
+        swagger.process_doc(self.spec, resource, list(urls))
+
+    def swagger_view(self):
+        class SwaggerView(restful.Resource):
+            api = self
+
+            def get(self):
+                return self.api.spec.to_dict()
+
+        return SwaggerView
---
+++ flask_guimauve/swagger.py
@@ -0,0 +1,66 @@
+from apispec.ext.marshmallow import swagger
+from functools import wraps
+
+def process_doc(spec, resource, urls):
+    operations = {}
+
+    for method in resource.methods:
+        method_name = method.lower()
+        method_view = getattr(resource, method_name)
+
+        # Define operation
+        operation = {}
+
+        if hasattr(method_view, '__doc__'):
+            operation['description'] = method_view.__doc__
+
+        # Define operation response
+        responses = {200: {}}
+
+        if hasattr(method_view, '__marshal_with__'):
+            res = method_view.__marshal_with__
+            process_schema(spec, res['schema'])
+            responses[200]['schema'] = res['schema']
+
+        operation['responses'] = responses
+
+        # Define operation parameters
+        if hasattr(method_view, '__accept__'):
+            accept = method_view.__accept__
+            process_schema(spec, accept['schema'])
+            operation['parameters'] = swagger.schema2parameters(accept['schema'], spec=spec, dump=False)
+
+        # Add operation
+        operations[method_name] = operation
+
+    for url in urls:
+        spec.add_path(url, operations=operations, view=None)
+
+def process_schema(spec, schema):
+    schema_cls = schema.__class__
+    name = '%s.%s' % (schema_cls.__module__, schema_cls.__qualname__)
+
+    spec.definition(name, schema=schema_cls)
+
+# Fix something in swagger spec generation
+# FIXME: Warning, black magic. Create a PR in apispec repo to fix this.
+def replace(orig_fn):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            return f(orig_fn, *args, **kwargs)
+        return wrapped
+    return wrapper
+
+@replace(swagger.field2property)
+def field2property(orig_fn, *args, **kwargs):
+    prop = orig_fn(*args, **kwargs)
+
+    field = args[0]
+    if field.dump_only:
+        prop['readOnly'] = True
+
+    return prop
+
+if swagger.field2property != field2property:
+    swagger.field2property = field2property
