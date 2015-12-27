f0427ce5acae0ac5a8189dab7d1452faf3a8befe
dusterherz <dusterherz@gmail.com>
2015-12-27 13:26:48 -0500

    Post work correctly. To add a monster, the server uses the current user and need the name of the monster to find it. See if this need some change or not.

--- nestedworld_api/views/api/v1/user/monster.py
+++ nestedworld_api/views/api/v1/user/monster.py
@@ -1,8 +1,9 @@
 from flask import jsonify, request
 from marshmallow import post_dump
 from nestedworld_api.app import ma
+from nestedworld_api.login import login_required, current_session
 from . import user
-from ..monster import Monster
+from nestedworld_api.db import Monster
 
 userMonster = user.namespace('monsters')
 
@@ -12,8 +13,8 @@ class UserMonster(userMonster.Resource):
     class Schema(ma.Schema):
         print(globals())
         id = ma.Integer(dump_only=True)
-        monster = ma.UrlFor("Monster")
-        user = ma.UrlFor("User")
+        monster = ma.String()
+        user = ma.String()
         surname = ma.String()
         experience = ma.Integer()
         level = ma.Integer()
@@ -36,7 +37,17 @@ class UserMonster(userMonster.Resource):
         from nestedworld_api.db import db
         from nestedworld_api.db import UserMonster
 
-        monster = UserMonster(**data)
+        choosedMonster = Monster.query.filter(
+            Monster.name == data['monster']).first()
+        if choosedMonster is None:
+            auth.abort(400, message='Monster not found')
+
+        monster = UserMonster()
+        monster.user = current_session.user
+        monster.monster = choosedMonster
+        monster.surname = data['surname']
+        monster.experience = data['experience']
+        monster.level = data['level']
 
         db.session.add(monster)
         db.session.commit()
