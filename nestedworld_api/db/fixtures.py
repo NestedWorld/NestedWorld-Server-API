0b12279161c96970cdfe31eb22a79836877f13ca
KokaKiwi <kokakiwi@kokakiwi.net>
2016-01-27 00:03:03 +0900

    Fix password checking

--- nestedworld_api/db/fixtures.py
+++ nestedworld_api/db/fixtures.py
@@ -13,18 +13,18 @@ def add(*objects):
 def reset_db():
     # Reset users
     admin = User(
-        email='kokakiwi@kokakiwi.net', password='kiwi3219',
-        pseudo='kokakiwi', city='Seoul', birth_date=arrow.get('1992-8-10'),
-        gender='male')
+        email='kokakiwi@kokakiwi.net', pseudo='kokakiwi', city='Seoul',
+        birth_date=arrow.get('1992-08-10'), gender='male')
+    admin.password = 'kiwi3291'
     florian = User(
-        email='florian.faisan@epitech.eu', password='florian',
-        pseudo='kassisdion', city='Seoul', birth_date=arrow.get('1993-9-12'),
-        gender='male')
+        email='florian.faisan@epitech.eu', pseudo='kassisdion', city='Seoul',
+        birth_date=arrow.get('1993-09-12'), gender='male')
+    florian.password = 'florian'
 
     alice = User(
-        email='alice@bob.com', password='bob',
-        pseudo='alice', city='CCrypto', birth_date=arrow.get('2010-10-10'),
-        gender='female')
+        email='alice@bob.com', pseudo='alice', city='CCrypto',
+        birth_date=arrow.get('2010-10-10'), gender='female')
+    alice.password = 'bob'
 
     add(admin, florian, alice)
 
