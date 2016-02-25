7d9e4ab2d2b6408e8842a93379dcc213169279bd
KokaKiwi <kokakiwi@kokakiwi.net>
2016-02-25 17:54:49 +0900

    Fix update script.

--- .update.sh
+++ .update.sh
@@ -3,6 +3,6 @@
 pip install --user -Ur requirements.txt
 pip install --user -Ur requirements_dev.txt
 
-./manage.py resetdb
-./manage.py import_monsters
-./manage.py import_places
+./manage.py db reset
+./manage.py db import_monsters
+./manage.py db import_places
