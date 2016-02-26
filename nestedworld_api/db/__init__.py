767f39793574d5be731d01434b6be7910f6266ab
dusterherz <dusterherz@gmail.com>
2016-02-26 00:47:01 -0500

    Places have now monsters

--- nestedworld_api/db/__init__.py
+++ nestedworld_api/db/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .attack import Attack
-from .geo import Place, Region, RegionMonster
+from .geo import Place, Region, RegionMonster, PlaceMonster
 from .token import Application, Session
 from .monster import Monster, MonsterAttack
 from .user import User, PasswordResetRequest, UserMonster, UserFriend, Inventory
--- nestedworld_api/views/api/v1/geo/monsters.py
+++ nestedworld_api/views/api/v1/geo/monsters.py
@@ -1,6 +1,34 @@
 from nestedworld_api.app import ma
 from . import places
 
+@places.route('/<place_id>/monsters')
+class PlaceMonsters(places.Resource):
+    tags = ['geo']
+
+    class Schema(ma.Schema):
+
+        class Infos(ma.Schema):
+            id = ma.Integer(dump_only=True)
+            url = ma.UrlFor('api.v1.monsters.monster', monster_id='<id>')
+
+            name = ma.String()
+            hp = ma.Float()
+            attack = ma.Float()
+            defense = ma.Float()
+
+        infos = ma.Nested(Infos, attribute='monster')
+
+    @places.marshal_with(Schema(many=True))
+    def get(self, place_id):
+        from nestedworld_api.db import PlaceMonster
+        from sqlalchemy.orm import joinedload
+
+        monsters = PlaceMonster.query\
+                                .filter(PlaceMonster.place_id == place_id)\
+                                .options(joinedload('monster'))\
+                                .all()
+        return monsters
+
 
 @places.route('/regions/<region_id>/monsters')
 class RegionMonsters(places.Resource):
