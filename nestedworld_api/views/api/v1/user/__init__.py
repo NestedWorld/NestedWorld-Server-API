aa47fd4577a2bba897535425b674a1a6fdb5f655
KokaKiwi <kokakiwi@kokakiwi.net>
2016-07-01 17:22:58 +0200

    Fix user conflict check on PUT for /users/

--- nestedworld_api/views/api/v1/user/__init__.py
+++ nestedworld_api/views/api/v1/user/__init__.py
@@ -11,6 +11,7 @@ from . import monsters
 from . import friends
 from . import inventory
 
+
 @user.route('/')
 class User(user.Resource):
     tags = ['users']
@@ -47,11 +48,9 @@ class User(user.Resource):
 
         user = current_session.user
 
-        conflict = User.query\
-                       .filter(User.id != user.id)\
-                       .filter((User.email == data['email']) | (User.pseudo == data['pseudo']))\
-                       .first()
-        if conflict is not None:
+        if 'email' in data and User.query.filter(User.email == data['email']).count() > 0:
+            user.abort(400, 'An user with same email/pseudo already exists')
+        if 'pseudo' in data and User.query.filter(User.pseudo == data['pseudo']).count() > 0:
             user.abort(400, 'An user with same email/pseudo already exists')
 
         for (name, value) in data.items():
